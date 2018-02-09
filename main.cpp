55ebcdae2a9c343fbf471559c2bfcc93f2d431bb
Danila Eremenko <danilaeremenko@mail.ru>
2018-02-09 21:16:40 +0300

    "Не хочу считать"(С)Seval

--- main.cpp
+++ main.cpp
@@ -2,73 +2,81 @@
 #include <stdio.h>
 #include "quanc8.h"
 #include "lagrange.h"
+#include "SPLINES.H"
 #include <math.h>
 #include <iostream>
 
-double x=0;
-double h=0.2;
-double t;
+double x = 0;
+const double max_x = 2;
+const double h = 0.2;
+const int nodeDigit = 11;
 
 // Функция (для Quanc8)
-double Function (double t)
-{
-	return exp(x*t)*sin(t);
+double Function(double t) {
+    return exp(x * t) * sin(t);
 }
 
 using namespace std;
 
-int main( void )
-{
-	double a=0, b=1, abserr=1.0e-12, relerr=0, errest, flag;
-	int nofun;
+int main(void) {
+    double a = 0, b = 1, abserr = 1.0e-12, relerr = 0, errest, flag;
+    int nofun;
 
-	// считаем при помощи QUANC8
-	x=0;
+    // считаем при помощи QUANC8
+    double ResultQUANC8[nodeDigit];
+    x = 0;
+    for (int i = 0; x < max_x; i++) {
+        quanc8(Function, a, b, abserr, relerr, &ResultQUANC8[i], &errest, &nofun, &flag);
+        x += h;
+    }
 
-	double ResultQUANC8[11];
-	for (int i=0; i<11; i++)
-	{
-		quanc8(Function, a, b, abserr, relerr, &ResultQUANC8[i], &errest, &nofun, &flag);
-		x+=h;
-	}
-	x=0;
+    // вычисл.Х[i],Y[i] для полинома Лагранжа
+    double lagrangeX[nodeDigit], lagrangeY[nodeDigit];
+    double ResultsLagr[nodeDigit];
+    x = 0;
+    for (int i = 0; x < max_x; i++) {
+        lagrangeX[i] = x;
+        x += h;
+    }
+    //TODO
+    x = 0;
+    for (int i = 0; x < max_x; i++) {
+        quanc8(Function, a, b, abserr, relerr, &lagrangeY[i], &errest, &nofun, &flag);
+        x += h;
+    }
+    //считаем при помощи полинома Лагранжа
+    x = 0;
+    for (int i = 0; x < max_x; i++) {
+        ResultsLagr[i] = lagrange(10, lagrangeX, lagrangeY, x);
+        x += h;
+    }
 
-	// вычисл.Х[i],Y[i] для полинома Лагранжа
-	double lagrangeX[11], lagrangeY[11];
-	double ResultsLagr[30];
-	double j=0;
-	
-	for (int i=0; i<=10; i++)
-	{
-		lagrangeX[i]=j;
-		j+=h;
-	}
-	for (int i=0; i<=10; i++)
-	{
-		quanc8(Function, a, b, abserr, relerr, &lagrangeY[i], &errest, &nofun, &flag);
-		x+=h;
-	}
-	//считаем при помощи полинома Лагранжа
-	double tr=0;
-	for (int i=0; i<11; i++)
-	{
-		ResultsLagr[i]=lagrange(10, lagrangeX, lagrangeY, tr);
-		tr+=h;
-	}
-
-	//Вычислим через Spline и Seval
-	double xs[11],ys[11],bs[11],cs[11],ds[11];
+    //Вычислим через Spline и Seval
+    double spl_x[nodeDigit], spl_y[nodeDigit], spl_b[nodeDigit], spl_c[nodeDigit], spl_s[nodeDigit];
+    double ResultSpline[nodeDigit];
+    for (int i = 0; x < 2; i++) {
+        spl_x[i] = x;
+        spl_y[i] = Function(x);
+        x += 0.2;
+    }
+    spline(nodeDigit, spl_x, spl_y, spl_b, spl_c, spl_s);
+    x = 0;
+    for (int i = 0; x < max_x; i++) {
+        ResultSpline[i] = seval(11, &x, spl_x, spl_y, spl_b, spl_c, spl_s);
+        x += h;
+    }
 
 
 
-//вывод 
-	cout<<"      QUANC8    Lagrange    Spline"<<endl;
-	double n=0;
-	for (int i=0; i<=10; i++)
-	{
-		cout<<n<<"  "<<ResultQUANC8[i]<<"  "<<ResultsLagr[i]<<"  "<<ResultsLagr[i]<<endl;;
-		n += h;
-	}
+
+    //Вывод
+    cout << "      QUANC8    Lagrange    Spline" << endl;
+    x = 0;
+
+    for (int i = 0; x < max_x; i++) {
+        cout << x << "  " << ResultQUANC8[i] << "  " << ResultsLagr[i] << "  " << ResultSpline[i] << endl;;
+        x += h;
+    }
 
     return 0;
 }
