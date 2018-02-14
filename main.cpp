94add7637b5249293e22fb465a0487db353567ae
danil <danilaeremenko@mail.ru>
2018-02-14 13:32:10 +0300

    Незначительный коммит

--- main.cpp
+++ main.cpp
@@ -24,47 +24,59 @@ using namespace std;
 int main() {
     //Исходные данные
     const double min_x = 0, max_x = 2, h = 0.2;
-    int nodeDigit = (max_x - min_x) / h;
+    const int firstNodeDigit = 1 + (max_x - min_x) / h;//Точки по которым строим полиномы
+    const int secondNodeDigit = 10;//Точки в которых вычисляем
 
     //QUANC8(функция, нижняя,верхняя,ABSERR,RELERR,результат, погрешность, кол-во вычислений функции, флаг)
     //Данные для QUANC8
     const int down = 0, up = 1;
     const double abserr = 1.0e-12, relerr = 0;
     double errest, flag;
-    int nofun[nodeDigit];
-    double x[nodeDigit], y[nodeDigit];//Узлы и значения функции в узлах
-    for (int i = 0; i < nodeDigit; ++i) {
+    int nofun[firstNodeDigit];
+    double x[firstNodeDigit], y[firstNodeDigit];//Узлы и значения функции в узлах
+    for (int i = 0; i < firstNodeDigit; ++i) {
         x[i] = h * i;
         current_x = x[i];
         quanc8(Function, down, up, abserr, relerr, &y[i], &errest, nofun, &flag);
     }
 
     //Построим Сплайн функицю и Полином Лагранжа
-    double splineFunction[nodeDigit], b[nodeDigit - 1], c[nodeDigit - 1], d[nodeDigit - 1];
-    double lagrangeFunction[nodeDigit];
-    double defaultFunction[nodeDigit];
+    double splineFunction[secondNodeDigit], b[secondNodeDigit], c[secondNodeDigit], d[secondNodeDigit];
+    double lagrangeFunction[secondNodeDigit];
+    double defaultFunction[secondNodeDigit];
 
-    spline(nodeDigit-1, x, y, b, c, d);
+    spline(secondNodeDigit, x, y, b, c, d);
 
-    for (int k = 0; k < nodeDigit; ++k) {
-        double xk=((k+1)-0.5)*h;
+    for (int k = 0; k < secondNodeDigit; ++k) {
+        double xk = ((k + 1) - 0.5) * h;
         current_x = xk;
         quanc8(Function, down, up, abserr, relerr, &defaultFunction[k], &errest, nofun, &flag);
-        lagrangeFunction[k] = lagrange(nodeDigit-1, x, y, xk);
-        splineFunction[k] = seval(nodeDigit-1, &xk, x, y, b, c, d);
+        lagrangeFunction[k] = lagrange(secondNodeDigit, x, y, xk);
+        splineFunction[k] = seval(secondNodeDigit, &xk, x, y, b, c, d);
     }
 
 
     printf("-----------------------------------------------------\n");
     printf("k |  xk   |    Values    |   Lagrange   |    Spline\n");
     printf("-----------------------------------------------------\n");
-    for (int k = 0; k < nodeDigit; ++k) {
-        printf("%-*.d| ", 2, k+1);
-        printf("%.2f | ", ((k+1)-0.5)*h);
-        printf("%.10f | ",defaultFunction[k]);
+    for (int k = 0; k < secondNodeDigit; ++k) {
+        printf("%-*.d| ", 2, k + 1);
+        printf("%.2f | ", ((k + 1) - 0.5) * h);
+        printf("%.10f | ", defaultFunction[k]);
         printf("%.10f | ", lagrangeFunction[k]);
         printf("%.10f", splineFunction[k]);
         printf("\n");
+    }
+
+    //Проверка погрешности
+    for (int i = 0; i < secondNodeDigit; ++i) {
+        double acceptValue = 0.1;
+        if (abs(defaultFunction[i] - lagrangeFunction[i]) > acceptValue ||
+            abs(defaultFunction[i] - splineFunction[i])>acceptValue) {
+            printf("Error in point k=" ,i);
+            printf("\n");
+        }
+
     }
 
 }
